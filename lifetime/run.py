78a1add5ebff0f6ddf3ec956c96d3d4f5c4b6e7e
seedindream <li.7533@buckeyemail.osu.edu>
2017-12-07 12:27:03 -0500

    third

--- lifetime/run.py
+++ lifetime/run.py
@@ -1,25 +1,10 @@
-import os
-import json
-import time
-import subprocess
-
-tm_st = time.time()
-
-cmdCommand = 'powershell D:\\home\\site\\wwwroot\\coldstart\\record.exe' #specify your cmd command
-process = subprocess.Popen(cmdCommand.split(), shell=True,stdout=subprocess.PIPE)
-output5, error = process.communicate()
-
-# postreqdata = json.loads(open(os.environ['req']).read())
-response = open(os.environ['res'], 'w')
-response.write("response time "+str(tm_st)+"@\n"+output5)
-response.close()
-
 import socket
 import json
 import os
 import subprocess
 import time
 import subprocess
+from urllib import urlopen
 
 env_dist = os.environ
 print env_dist.get('WEBSITE_INSTANCE_ID')
